3cbf0ea5ef11ca37e7c8c95e993e46c7d639a005
kierstenv <kierstenvicknair22@gmail.com>
2022-02-24 22:40:34 -0600

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
+
 // just so i can use it in function ?
 let taskFormHandler = (event) => {
   event.preventDefault();
   let taskNameInput = document.querySelector("input[name='task-name']").value;
   let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  //after return it just goes back 2 addEventListener(submit, taskFormHandler)
+  // - like the caller function is false so it stops ???
+  // then regoes bc its the only call rn ????
+
+  formEl.reset();
 
   let taskDataObj = {
     name: taskNameInput,
@@ -13,6 +24,7 @@ let taskFormHandler = (event) => {
 
   createTaskEl(taskDataObj);
 };
+
 // y have 2 use both object in function and argument/parameter ?
 let createTaskEl = (taskDataObj) => {
   let taskItemEl = document.createElement("li");
@@ -21,6 +33,7 @@ let createTaskEl = (taskDataObj) => {
   let taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  // HOW 2 PREVENT CROSS SITE SCRIPTIN G IM GONNA SCREAM
   taskItemEl.appendChild(taskInfoEl);
   
   tasksToDoEl.appendChild(taskItemEl);
