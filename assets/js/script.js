9a63888d63a1cb3ebd50a3d509b762cf0674cb2b
kierstenv <kierstenvicknair22@gmail.com>
2022-02-24 21:50:22 -0600

    add task name and type

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,18 @@ let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 let createTaskHandler = (event) => {
   event.preventDefault();
+  let taskNameInput = document.querySelector("input[name='task-name']").value;
+  let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
   let taskItemEl = document.createElement("li");
   taskItemEl.className = "task-item";
-  taskItemEl.textContent = "New Task";
+
+  let taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskItemEl.appendChild(taskInfoEl);
+
   tasksToDoEl.appendChild(taskItemEl);
 };
 
