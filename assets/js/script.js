091c881bc8d11bd542fc8f3528b5d669541c213a
kierstenv <kierstenvicknair22@gmail.com>
2022-02-24 22:15:25 -0600

    refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,30 @@
 let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
-
-let createTaskHandler = (event) => {
+// just so i can use it in function ?
+let taskFormHandler = (event) => {
   event.preventDefault();
   let taskNameInput = document.querySelector("input[name='task-name']").value;
   let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  let taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+
+  createTaskEl(taskDataObj);
+};
+// y have 2 use both object in function and argument/parameter ?
+let createTaskEl = (taskDataObj) => {
   let taskItemEl = document.createElement("li");
   taskItemEl.className = "task-item";
-
+  
   let taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
-
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   taskItemEl.appendChild(taskInfoEl);
-
+  
   tasksToDoEl.appendChild(taskItemEl);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
